5700d91f3178ff5f1f6988ef9bdcbc0026294c2c
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-17 20:40:05 -0300

    Ativa V1.2.1 com biblioteca de mídia gerenciada

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from english_player.v120_window import MainWindowV120
+from english_player.v121_window import MainWindowV121
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV120()
+    window = MainWindowV121()
     window.resize(1320, 840)
     window.show()
     sys.exit(app.exec())
