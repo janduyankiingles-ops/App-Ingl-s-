27c95a1587779412df85c7d14669b0c6277f47aa
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-17 19:54:15 -0300

    Ativa V0.9 no aplicativo

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from english_player.v080_window import MainWindowV080
+from english_player.v090_window import MainWindowV090
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV080()
+    window = MainWindowV090()
     window.resize(1320, 840)
     window.show()
     sys.exit(app.exec())
