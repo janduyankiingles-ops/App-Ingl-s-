a3092233e45723642263d47c6799f93ebc8c371d
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-17 18:21:09 -0300

    V0.5.5: ativa modo offline

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from english_player.v054_window import MainWindowV054
+from english_player.v055_window import MainWindowV055
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV054()
+    window = MainWindowV055()
     window.resize(1320, 840)
     window.show()
     sys.exit(app.exec())
