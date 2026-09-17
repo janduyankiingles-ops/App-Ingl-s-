4d1d51d2431d7d68420cc0e26171bd6badb1898f
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-17 19:06:01 -0300

    V0.6.1: ativa janela do dicionário expandido

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from english_player.v060_window import MainWindowV060
+from english_player.v061_window import MainWindowV061
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV060()
+    window = MainWindowV061()
     window.resize(1320, 840)
     window.show()
     sys.exit(app.exec())
