c458409d56f1d3211e84b5322e6ae6cc8696f5ae
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-17 20:16:49 -0300

    Ativa V1.0 Painel de Progresso

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from english_player.v092_window import MainWindowV092
+from english_player.v100_window import MainWindowV100
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV092()
+    window = MainWindowV100()
     window.resize(1320, 840)
     window.show()
     sys.exit(app.exec())
