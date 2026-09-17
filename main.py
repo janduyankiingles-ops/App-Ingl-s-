0972cd6607dfcab699f26ad83f4a309f065af395
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-17 19:27:57 -0300

    V0.7.1: ativa nova janela principal

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from english_player.v070_window import MainWindowV070
+from english_player.v071_window import MainWindowV071
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV070()
+    window = MainWindowV071()
     window.resize(1320, 840)
     window.show()
     sys.exit(app.exec())
