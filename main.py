7bb9c2e1517262612d01cda8687e4187caa0277d
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-17 20:46:35 -0300

    Ativa V1.2.2 com importação melhorada de séries

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from english_player.v121_window import MainWindowV121
+from english_player.v122_window import MainWindowV122
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV121()
+    window = MainWindowV122()
     window.resize(1320, 840)
     window.show()
     sys.exit(app.exec())
