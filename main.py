127e66c578449ad9b45f925bde8bd0061ec43340
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-18 19:47:01 -0300

    Ativa V1.7.0 com Frases Inteligentes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from english_player.v137_window import MainWindowV137
+from english_player.v138_window import MainWindowV138
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV137()
+    window = MainWindowV138()
     window.resize(1500, 900)
     window.show()
     sys.exit(app.exec())
