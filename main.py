29473d739b885e0bac0bf0d76761bbc3ae1b7467
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-18 18:42:34 -0300

    Ativa V1.3 Música

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from english_player.v125_window import MainWindowV125
+from english_player.v130_window import MainWindowV130
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV125()
+    window = MainWindowV130()
     window.resize(1320, 840)
     window.show()
     sys.exit(app.exec())
