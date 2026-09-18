197c0abaa231bb0e771801ee6c6d6b39ab3ad8bd
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-18 20:07:13 -0300

    Ativa V2.1.0 com aba Filmes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from english_player.v200_window import MainWindowV200
+from english_player.v210_window import MainWindowV210
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV200()
+    window = MainWindowV210()
     window.resize(1500, 900)
     window.show()
     sys.exit(app.exec())
