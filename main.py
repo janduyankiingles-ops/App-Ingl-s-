78d10458425e4b8fd9d963fc7d5e87847741d8ca
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-23 19:20:36 -0300

    V2.8.0: ativa layout responsivo otimizado

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-from english_player.v272_window import MainWindowV272
+from english_player.v280_window import MainWindowV280
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV272()
+    window = MainWindowV280()
     window.resize(1500, 900)
     window.show()
     sys.exit(app.exec())
