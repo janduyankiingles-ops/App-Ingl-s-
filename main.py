97b9fce990cf18562b1cd3ac34c3c33755e2a44d
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-23 19:56:45 -0300

    V2.9.4: ativa hotfix estável

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-from english_player.v292_window import MainWindowV292
+from english_player.v294_window import MainWindowV294
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV292()
+    window = MainWindowV294()
     window.resize(1500, 900)
     window.show()
     sys.exit(app.exec())
