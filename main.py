7bca253f7aeeda9ff8623872fee51af8f3d9be74
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-23 19:33:52 -0300

    V2.9.0: ativa normalização responsiva global

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-from english_player.v280_window import MainWindowV280
+from english_player.v290_window import MainWindowV290
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV280()
+    window = MainWindowV290()
     window.resize(1500, 900)
     window.show()
     sys.exit(app.exec())
