568c5abb3281a476120fea47893558f4f428185b
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-23 19:56:26 -0300

    V2.9.4: restaura base estável da V2.8

---
+++ english_player/v294_window.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from .v280_window import MainWindowV280
+
+
+class MainWindowV294(MainWindowV280):
+    """V2.9.4: hotfix de estabilidade; remove a camada global V2.9."""
+
+    pass
