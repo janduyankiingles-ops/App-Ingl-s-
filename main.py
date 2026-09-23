3149ce185e409d4cf00b3bba5026e821012399be
janduyankiingles-ops <janduyankiingles@gmail.com>
2026-09-23 17:16:22 -0300

    V2.3: ativa aba Texto

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-from english_player.v220_window import MainWindowV220
+from english_player.v230_window import MainWindowV230
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("English Video Player")
-    window = MainWindowV220()
+    window = MainWindowV230()
     window.resize(1500, 900)
     window.show()
     sys.exit(app.exec())
